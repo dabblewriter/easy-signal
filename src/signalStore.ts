e7674f906de70449c761e6c4e2fa5a0c4e89cc69
Jacob Wright <jacwright@gmail.com>
2024-04-10 15:12:49 -0600

    Adding better type to store version

--- src/signalStore.ts
+++ src/signalStore.ts
@@ -1,10 +1,17 @@
-import { ReactiveSignal, ReactiveSignalSubscriber, subscribe } from './reactiveSignal';
+import { ReactiveSignal, ReactiveSignalSubscriber, Unsubscribe, subscribe } from './reactiveSignal';
+
+export interface SignalStore<T> {
+  get(): T;
+  set(value: T): void;
+  update(updater: (value: T) => T): void;
+  subscribe(sub: (value: T) => void): Unsubscribe;
+}
 
 /**
  * A store wrapper around a reactive signal. The store can be used to get, set, update, and subscribe to the signal.
  * This is for use in Svelte 3-4.
  */
-export function signalStore<T>(signal: ReactiveSignal<T>) {
+export function signalStore<T>(signal: ReactiveSignal<T>): SignalStore<T> {
   return {
     get: signal,
     set: signal,
