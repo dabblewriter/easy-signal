ee86a415eda9e5592b4eded3a1ef89878aa3c52f
Jacob Wright <jacwright@gmail.com>
2024-04-10 15:21:08 -0600

    Differentiate between read and write stores

--- src/signalStore.ts
+++ src/signalStore.ts
@@ -1,21 +1,27 @@
-import { ReactiveSignal, ReactiveSignalSubscriber, Unsubscribe, subscribe } from './reactiveSignal';
+import { ComputedSignal, ReactiveSignal, Unsubscribe } from './reactiveSignal';
 
-export interface SignalStore<T> {
+export interface SignalReadStore<T> {
   get(): T;
+  subscribe(sub: (value: T) => void): Unsubscribe;
+}
+
+export interface SignalWriteStore<T> extends SignalReadStore<T> {
   set(value: T): void;
   update(updater: (value: T) => T): void;
-  subscribe(sub: (value: T) => void): Unsubscribe;
 }
 
 /**
  * A store wrapper around a reactive signal. The store can be used to get, set, update, and subscribe to the signal.
  * This is for use in Svelte 3-4.
  */
-export function signalStore<T>(signal: ReactiveSignal<T>): SignalStore<T> {
+export function signalStore<T>(signal: ComputedSignal<T>): SignalReadStore<T>;
+export function signalStore<T>(signal: ReactiveSignal<T>): SignalWriteStore<T>;
+export function signalStore<T>(signal: ReactiveSignal<T> | ComputedSignal<T>): SignalReadStore<T> | SignalWriteStore<T> {
+  const isComputed = signal.length === 0;
   return {
     get: signal,
-    set: signal,
-    update: signal,
-    subscribe: (sub: ReactiveSignalSubscriber<T>) => subscribe(signal, sub),
+    set: !isComputed && signal,
+    update: !isComputed && signal,
+    subscribe: signal.subscribe,
   };
 }
